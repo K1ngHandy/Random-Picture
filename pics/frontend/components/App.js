8cc652baaa4ca7a9cbbc2c70d4e6109e61fd1551
K1ngHandy <bolds-slicks0g@icloud.com>
2025-01-08 22:08:38 -0500

    add styled components

--- pics/frontend/components/App.js
+++ pics/frontend/components/App.js
@@ -1,10 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: cyan;
+  border-radius: 3px;
+  border: 2px solid #BF4F74;
+  color: #BF4F74;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
+
+const WrapperDiv = styled.div`
+  width: 80%;
+  height: 80%;
+  background-color: gainsboro;
+  margin: auto;
+`;
 
 function App() {
   const [pic, setPic] = useState('');
   const [info, setInfo] = useState('')
   const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const refreshPage = () => {
+    window.location.reload();
+  }
 
   useEffect(() => {
     const handleMouseMove = e => setPosition({ x: e.clientX, y: e.clientY });
@@ -23,13 +44,15 @@ function App() {
   }, []);
 
   return (
-    <div className='App'>
+    <WrapperDiv className='App'>
       <h1>Great picture!</h1>
       <img src={pic} height='300px' alt='alt text' />
       <h2>{info}</h2>
+      <Button onClick={refreshPage}>Random Picture</Button>
+      
       <h3>Mouse position:</h3>
       {position.x}:{position.y}
-    </div>
+    </WrapperDiv>
   )
 }
 
